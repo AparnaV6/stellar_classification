dc4b766a4941efbc5259b597cf56595dcaee24f0
AparnaV6 <117962649+AparnaV6@users.noreply.github.com>
2024-06-03 20:47:51 +0530

    Update stellar_pipeline.py

--- stellar_pipeline.py
+++ stellar_pipeline.py
@@ -7,79 +7,114 @@ Original file is located at
     https://colab.research.google.com/drive/1a8f_gtX_KXNVQX8YDhe2KLSq3mJiuVG2
 """
 
-import numpy as np
-import pandas as pd
-import pickle
-import os
 import random
-import cv2
 import plotly.express as px
+import pickle
+import pandas as pd
+import os
+import numpy as np
+import cv2 as cv
+import warnings
+warnings.filterwarnings('ignore')
+
 
 class Pipeline(object):
-  def __init__(self, data):
-    self.data = data
-    self.features = ['alpha', 'delta', 'u', 'g', 'r', 'i', 'z', 'redshift']
-    self.label = 'class'
-    self.df = None
+    """
+    This class is a pipeline mechanism to feed the 
+    query datapoint into the model for prediction.
+    """
 
-  def get_input(self):
-    self.df = pd.DataFrame(data=self.df, columns=self.features)
+    def __init__(self, data) -> None:
+        self.data = data
+        self.features = ['alpha', 'delta', 'u', 'g', 'r', 'i', 'z', 'redshift']
+        self.target = 'class'
+        self.df = None
 
-  def preprocessing(self):
-    with open(file='scaling.pkl', mode='rb') as scale:
-      scaler = pickle.load(file=scale)
+    def fetch(self) -> None:
+        """
+        This method fetches the data.
+        """
+        self.df = pd.DataFrame(data=self.data, columns=self.features)
 
-    self.df = scaler.transform(x=self.df)
-    self.df = pd.DataFrame(data=self.df, columns=self.features)
+    def preprocess(self) -> None:
+        """
+        This method preprocesses the data.
+        """
+        with open(file='scaling.pkl', mode='rb') as s_pkl:
+            scaling = pickle.load(file=s_pkl)
 
-  def prediction(self):
-    with open(file='stacking_classifier.pkl', mode='rb') as classify:
-      classifier = pickle.load(file=classify)
+        self.df = scaling.transform(X=self.df)
+        self.df = pd.DataFrame(data=self.df, columns=self.features)
 
-    prob = classifier.predict_proba(x=self.df)
-    confidence = np.round(a=np.max(a=prob)*100, decimals=2)
-    prediction = classifier.predict(x=self.df)[0]
+    def featurize(self) -> None:
+        """
+        This method performs feature engineering on the data.
+        """
+        fi_cols = ['redshift', 'g-r', 'i-z', 'u-r', 'i-r', 'z-r', 'g']
+        self.df['g-r'] = self.df['g'] - self.df['r']
+        self.df['i-z'] = self.df['i'] - self.df['z']
+        self.df['u-r'] = self.df['u'] - self.df['r']
+        self.df['i-r'] = self.df['i'] - self.df['r']
+        self.df['z-r'] = self.df['z'] - self.df['r']
+        self.df = self.df[fi_cols]
 
-    fig = self.get_image(prediction=prediction)
+    def predict(self) -> tuple:
+        """
+        This method predicts the query datapoint.
+        """
+        with open(file='stacking_classifier.pkl', mode='rb') as m_pkl:
+            clf = pickle.load(file=m_pkl)
 
-    if prediction=="QSO":
-      prediction = "Quasi-Stellar Object"
-    elif prediction=="GALAXY":
-      prediction = "Galaxy"
-    else:
-      prediction = "Star"
+        pred_proba = clf.predict_proba(X=self.df)
+        confidence = np.round(a=np.max(a=pred_proba)*100, decimals=2)
+        pred_class = clf.predict(X=self.df)[0]
 
-    result = "Predicted class: '{}' with a confidence of {}%".format(prediction, confidence)
-    return result, fig, prediction
+        fig = self.get_encoded_image(pred_class=pred_class)
 
-  def get_image(self, prediction):
-    image_root = os.path.join(os.getcwd(), 'path')
+        if pred_class == 'QSO':
+            pred_class = 'Quasi-Stellar Object'
+        elif pred_class == 'GALAXY':
+            pred_class = 'Galaxy'
+        else:
+            pred_class = 'Star'
 
-    if prediction == 'QSO':
-      image_class_path = os.path.join(image_root, 'qsos')
-    elif prediction == 'GALAXY':
-      image_class_path = os.path.join(image_root, 'galaxies')
-    else:
-      image_class_path = os.path.join(image_root, 'stars')
+        c = "The class is '{}' with a confidence of {}%.".format(
+            pred_class, confidence)
+        return c, fig, pred_class
 
-    image_picked = random.choice(seq=os.listdir(path=image_class_path))
-    image_picked_path = os.path.join(image_class_path, image_picked)
+    def get_encoded_image(self, pred_class):
+        """
+        This method selects an image and encodes it.
+        """
+        image_root = os.path.join(os.getcwd(), './images')
+        if pred_class == 'QSO':
+            image_class_path = os.path.join(image_root, 'qsos')
+        elif pred_class == 'GALAXY':
+            image_class_path = os.path.join(image_root, 'galaxies')
+        else:
+            image_class_path = os.path.join(image_root, 'stars')
+        image_picked = random.choice(seq=os.listdir(path=image_class_path))
+        image_picked_path = os.path.join(image_class_path, image_picked)
 
-    image = cv2.imread(filename=image_picked_path)
-    image = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2RGB)
+        image = cv.imread(filename=image_picked_path)
+        image = cv.cvtColor(src=image, code=cv.COLOR_BGR2RGB)
 
-    image_fig = px.imshow(img=image)
-    image_fig.update_layout(
-        coloraxis_showscale=False,
-        autosize=True, height=500,
-        margin=dict(l=0, r=0, b=0, t=0)
-    )
-    image_fig.update_xaxes(showticklabels=False)
-    image_fig.update_yaxes(showticklabels=False)
-    return image_fig
+        image_fig = px.imshow(img=image)
+        image_fig.update_layout(
+            coloraxis_showscale=False,
+            autosize=True, height=500,
+            margin=dict(l=0, r=0, b=0, t=0)
+        )
+        image_fig.update_xaxes(showticklabels=False)
+        image_fig.update_yaxes(showticklabels=False)
+        return image_fig
 
-  def pipeline(self):
-    self.get_input()
-    self.preprocessing()
-    return self.predict()
+    def pipeline(self) -> str:
+        """
+        This method is a pipeline.
+        """
+        self.fetch()
+        self.preprocess()
+        self.featurize()
+        return self.predict()
 
